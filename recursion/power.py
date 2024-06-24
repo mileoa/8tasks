5fbd94f7e673a3549a32f41530534f05eed3768c
mileoa <ezkimozzzzz@gmail.com>
2024-06-24 21:28:35 +1000

    Исправил форматирование

--- recursion/power.py
+++ recursion/power.py
@@ -1,7 +1,7 @@
-def power(N:int, M: int) -> int | float:
+def power(N: int, M: int) -> int | float:
     """Return N to the power of M."""
     if M == 0:
         return 1
     if M < 0:
-        return 1 / pow (N, -M)
+        return 1 / pow(N, -M)
     return N * pow(N, M - 1)
