9ef69914b607f96d56a7ee410dfcb17b130e1279
mileoa <ezkimozzzzz@gmail.com>
2024-06-26 00:12:01 +1000

    Исправил docstring

--- recursion/print_even_indexes.py
+++ recursion/print_even_indexes.py
@@ -2,12 +2,12 @@ from typing import Any
 
 
 def print_even_indexes(l: list[Any]) -> None:
-    """Print elements on even indexes."""
+    """Print elements at even indexes."""
     return print_even_indexes_recursive(l, 0)
 
 
 def print_even_indexes_recursive(l: list[Any], i: int) -> None:
-    """Print elements on even indexes."""
+    """Print elements at even indexes."""
     if i >= len(l):
         return None
     print(l[i])
