cd9781ff069b1b617bfb2c3c786d5065dcbf00d1
mileoa <ezkimozzzzz@gmail.com>
2024-06-26 09:34:17 +1000

    Исправил форматирование

--- recursion/power.py
+++ recursion/power.py
@@ -1,17 +1,12 @@
 def power(N: int, M: int) -> int | float:
     """Return N to the power of M."""
-    M_negative: bool = M < 0
-    if M_negative:
-        M = -M
-    return power_recursive(N, M, M_negative, 1)
+    if M < 0:
+        return 1 / power_recursive(N, -M, 1)
+    return power_recursive(N, M, 1)
 
 
-def power_recursive(
-    N: int, M: int, M_negative: bool, product: int | float
-) -> int | float:
+def power_recursive(N: int, M: int, product: int | float) -> int | float:
     """Return N to the power of M."""
     if M == 0:
-        if M_negative:
-            return 1 / product
         return product
-    return power_recursive(N, M - 1, M_negative, product * N)
+    return power_recursive(N, M - 1, product * N)
