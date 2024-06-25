3a586ae93079059d1edf5c9b9dfda70b75fb0ff2
mileoa <ezkimozzzzz@gmail.com>
2024-06-25 17:18:58 +1000

    Исправил ошибку.

--- recursion/list_len.py
+++ recursion/list_len.py
@@ -3,8 +3,7 @@ from typing import Any
 
 def list_len(l: list[Any]) -> int:
     """Return lenght of list."""
-    try:
-        l.pop(0)
-        return 1 + list_len(l)
-    except IndexError:
+    if len(l) == 0:
         return 0
+    l.pop(0)
+    return 1 + list_len(l)
