28086f9b8c97ccd12ecb043ded527bf5494d9c87
mileoa <ezkimozzzzz@gmail.com>
2024-06-25 17:35:08 +1000

    Исправил ошибку.

--- recursion/is_palindrome.py
+++ recursion/is_palindrome.py
@@ -1,5 +1,10 @@
-def is_palindrome(s: str, n: int) -> bool:
-    """Return whether given string is polyndrom."""
+def is_palindrome(s: str) -> bool:
+    """Return whether given string is palindrom."""
+    return is_palindrome_recursive(s, len(s))
+
+
+def is_palindrome_recursive(s: str, n: int) -> bool:
+    """Return whether given string is palindrom."""
     if n == int(len(s) // 2):
         return True
-    return s[len(s) - n] == s[n - 1] and is_palindrome(s, n - 1)
+    return s[len(s) - n] == s[n - 1] and is_palindrome_recursive(s, n - 1)
