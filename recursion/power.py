274e079a27f0ec151238b50bc615c84459374b62
mileoa <ezkimozzzzz@gmail.com>
2024-06-26 00:06:11 +1000

    Изменил логику.

--- recursion/power.py
+++ recursion/power.py
@@ -1,7 +1,17 @@
 def power(N: int, M: int) -> int | float:
     """Return N to the power of M."""
+    M_negative: bool = M < 0
+    if M_negative:
+        M = -M
+    return power_recursive(N, M, M_negative, 1)
+
+
+def power_recursive(
+    N: int, M: int, M_negative: bool, product: int | float
+) -> int | float:
+    """Return N to the power of M."""
     if M == 0:
-        return 1
-    if M < 0:
-        return 1 / pow(N, -M)
-    return N * pow(N, M - 1)
+        if M_negative:
+            return 1 / product
+        return product
+    return power_recursive(N, M - 1, M_negative, product * N)
--- recursion/sum_digits.py
+++ recursion/sum_digits.py
@@ -1,5 +1,10 @@
 def sum_digits(N: int) -> int:
     """Return sum of digits."""
+    return sum_digits_recursive(N, 0)
+
+
+def sum_digits_recursive(N: int, result: int) -> int:
+    """Return sum of digits."""
     if N == 0:
-        return 0
-    return N % 10 + sum_digits(N // 10)
+        return result
+    return sum_digits_recursive(N // 10, result + N % 10)
