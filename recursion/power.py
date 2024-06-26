8fc326f84d5a84d164260a93d2d6b102c3f2a037
mileoa <ezkimozzzzz@gmail.com>
2024-06-26 11:50:29 +1000

    Изменил формат

--- recursion/power.py
+++ recursion/power.py
@@ -1,7 +1,5 @@
 def power(N: int, M: int) -> int | float:
     """Return N to the power of M."""
-    if M < 0:
-        return 1 / power_recursive(N, -M, 1)
     return power_recursive(N, M, 1)
 
 
@@ -9,4 +7,10 @@ def power_recursive(N: int, M: int, product: int | float) -> int | float:
     """Return N to the power of M."""
     if M == 0:
         return product
-    return power_recursive(N, M - 1, product * N)
+    if M < 0:
+        product *= 1 / N
+        M += 1
+    else:
+        product *= N
+        M -= 1
+    return power_recursive(N, M, product)
