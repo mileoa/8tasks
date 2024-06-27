157e805e1e94bd2c7c7e60f9672ec68dcbfc18fe
mileoa <ezkimozzzzz@gmail.com>
2024-06-27 14:58:03 +1000

    Переписал для использования 1 функции

--- recursion/get_all_files.py
+++ recursion/get_all_files.py
@@ -1,30 +1,12 @@
 import os.path
 
 
-def get_all_files(folder_path: str) -> list[str]:
+def get_all_files(element_path: str) -> list[str]:
     """Return all file names in given path including subfolders."""
-    return get_all_files_recursive([folder_path], 0, [])
-
-
-def get_all_files_recursive(
-    elements_to_check: list[str], i: int, result: list[str]
-) -> list[str]:
-    if i == len(elements_to_check):
+    result: list[str] = []
+    if os.path.isfile(element_path):
+        result.append(os.path.split(element_path)[1])
         return result
-    if os.path.isdir(elements_to_check[i]):
-        elements_to_check.extend(
-            [
-                os.path.join(elements_to_check[i], j)
-                for j in os.listdir(elements_to_check[i])
-            ]
-        )
-    elif os.path.isfile(elements_to_check[i]):
-        result.append(os.path.split(elements_to_check[i])[1])
-    return get_all_files_recursive(
-        elements_to_check,
-        i + 1,
-        result,
-    )
-
-
-print(get_all_files("."))
+    for i in os.listdir(element_path):
+        result.extend(get_all_files(os.path.join(element_path, i)))
+    return result
