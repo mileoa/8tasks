900b3b951064fa853f3f26e2a8c837b39c6dc79e
mileoa <ezkimozzzzz@gmail.com>
2024-07-01 20:41:31 +1000

    Сделал алгоритм более универсальным

--- EEC_help/EEC_help.py
+++ EEC_help/EEC_help.py
@@ -1,20 +1,27 @@
 def EEC_help(arr1: list[int], arr2: list[int]) -> bool:
     """Return whether arr1 contain same elements as arr2."""
-    return EEC_help_recursive(arr1, arr2, 0, [0, 0, 0, 0, 0, 0, 0, 0, 0])
+    return EEC_help_recursive(arr1, arr2, 0, {})
 
 
 def EEC_help_recursive(
-    arr1: list[int], arr2: list[int], i: int, arr_calc: list[int]
+    arr1: list[int], arr2: list[int], i: int, arr_calc: dict[int, int]
 ) -> bool:
     """Return whether arr1 contain same elements as arr2."""
     if len(arr1) != len(arr2):
         return False
     if i == len(arr1):
-        if arr_calc.count(0) != 9:
+        if list(arr_calc.values()).count(0) != len(arr_calc):
             return False
         return True
 
-    arr_calc[arr1[i] - 1] += 1
-    arr_calc[arr2[i] - 1] -= 1
+    if arr1[i] in arr_calc:
+        arr_calc[arr1[i]] += 1
+    else:
+        arr_calc[arr1[i]] = 1
+
+    if arr2[i] in arr_calc:
+        arr_calc[arr2[i]] -= 1
+    else:
+        arr_calc[arr2[i]] = -1
 
     return EEC_help_recursive(arr1, arr2, i + 1, arr_calc)
